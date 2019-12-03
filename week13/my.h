2a164490e97802f7957866b4ec23260262256527
comewei <871321767@qq.com>
2019-12-02 17:16:16 -0800

    20191204

---
+++ week13/my.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <wait.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+
